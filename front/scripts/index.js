76a57c7b6f9248ac1afa1f848a582419199d7b4f
MatiasGEscobar <matiie9135@gmail.com>
2024-10-16 11:22:49 -0300

    modif Hito N°6 (HW)

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -6,8 +6,8 @@ const axios = require ("axios");
 
 const fetchMovies = async() => {
   try{
-    const data = await axios.get("https://students-api.up.railway.app/movies");
-    const arrayHtmlCard = data.data.map(CreateElement);
+    const {data} = await axios.get("https://students-api.up.railway.app/movies");
+    const arrayHtmlCard = data.map(CreateElement);
     arrayHtmlCard.forEach((movie) => movieContainer.appendChild(movie));
   }catch (err){
     alert("excepción en cargar los archivos desde la API, se utilizan los datos guardados");
