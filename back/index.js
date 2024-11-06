699da7992e9f8664261b1b1b32fda06fa75ac52c
MatiasGEscobar <matiie9135@gmail.com>
2024-11-06 10:24:39 -0300

    1

--- back/index.js
+++ back/index.js
@@ -1,12 +1,13 @@
 const connectDB = require ("./src/config/dbConfig.js");
 const app = require ("./src/server.js");
 
-connectDB().then(() => {
+connectDB()
+.then(() => {
     console.log("Conexión a la base de datos establecida");
     app.listen(3000, () => {
         console.log("Server corriendo en puerto 3000");
     })  
 })
-    .catch((error) => { 
-        console.log(error);
-    })
+.catch((error) => { 
+    console.log(error);
+})
